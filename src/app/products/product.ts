da884652e4b3cb4ede8ac0ccd6ed5d799999b6e5
athira5ivan <athirayes246@gmail.com>
2022-07-16 11:44:32 +0530

    intial commit

---
+++ src/app/products/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    constructor(public productId: number,
+        public productName: string,
+        public productCode: string,
+        public price: number,
+        public description: string,
+        public imageUrl: string,
+        public manufacturer: string,
+        public rating: number) {
+    }
+}
